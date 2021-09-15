7089009d885894a87ba0330b17f3aeeadefb1c61
Ewertton Silva <ewertton45mb@gmail.com>
2021-09-15 15:38:37 -0300

    Percorrendo lista e dicionário

---
+++ data_types.py
@@ -0,0 +1,19 @@
+#Percorrendo uma lista
+lista = ['Luiz', 10, 'Maria', 22, 'Lucas']
+
+for item in lista:
+    i = item
+    print(i)
+
+print('---------------------')
+
+#Percorrendo um dicionário
+dicts = {
+    "nome": "Luiz",
+    "idade": 24,
+    "altura": 1.90
+}
+
+for pessoa in dicts:
+    p = dicts[pessoa]
+    print(p)
